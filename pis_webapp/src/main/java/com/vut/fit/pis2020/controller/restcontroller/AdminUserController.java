38ee5125c4036eb88828fba486c1bae33560a771
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2020-04-21 17:29:54 +0200

    Add: Admin user API create

--- pis_webapp/src/main/java/com/vut/fit/pis2020/controller/restcontroller/AdminUserController.java
+++ pis_webapp/src/main/java/com/vut/fit/pis2020/controller/restcontroller/AdminUserController.java
@@ -5,11 +5,10 @@ import com.vut.fit.pis2020.dto.UserDto;
 import com.vut.fit.pis2020.entity.User;
 import com.vut.fit.pis2020.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -40,5 +39,24 @@ public class AdminUserController {
         User user = userService.findById(userId);
 
         return userDtoConverter.convertToUserDto(user);
+    }
+
+    @PostMapping("/api/admin/users/create")
+    public HashMap<String, String> createUser(@ModelAttribute("user") UserDto userDto) {
+
+        HashMap<String, String> returnCode = new HashMap<>();
+
+        if(userService.findByEmail(userDto.getEmail()) != null) {
+            returnCode.put("400", "There is already a user registered with the email provided");
+
+            return returnCode;
+        }
+
+        User user = userDtoConverter.convertToUser(userDto);
+        userService.save(user);
+
+        returnCode.put("200", "User registered");
+
+        return returnCode;
     }
 }
